83af3a114d20be0f2e259e1fc3c812bd3d8b9916
Emi Simpson <emi@alchemi.dev>
2024-11-26 14:47:06 -0500

    Add a demo

---
+++ demo/src/Main.elm
@@ -0,0 +1,40 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Kdl.Parse exposing (parse)
+import Kdl.Serialize exposing (serialize)
+import Kdl.Util exposing (k)
+
+main = Browser.sandbox
+    { init = "myNode 1 2 3 key=\"val\" {child;}"
+    , update = k
+    , view = view
+    }
+
+view : String -> Html String
+view s =
+    let
+        rightText = case parse s of
+            Ok kdl -> serialize kdl
+            Err e -> Debug.toString e
+    in div
+        [ style "display" "grid"
+        , style "grid-template-columns" "1fr 1fr"
+        , style "height" "100vh"
+        ]
+        [ textarea
+            [ onInput identity
+            , style "font-family" "mono"
+            ]
+            [ text s
+            ]
+        , textarea
+            [ readonly True
+            , style "font-family" "mono"
+            ]
+            [ text rightText
+            ]
+        ]
